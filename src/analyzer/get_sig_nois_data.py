51d95f5a6e1d95c2f94d81a9bdaa91a76ee98bee
John Onwuemeka <john.onwuemeka@192.168.1.21>
2019-05-15 21:08:44 -0400

    Update analyzer files

--- src/analyzer/get_sig_nois_data.py
+++ src/analyzer/get_sig_nois_data.py
@@ -6,9 +6,10 @@ Created on Mon Apr 22 09:39:20 2019
 @author: john.onwuemeka
 """
 from obspy.core import read
+from .remove_ir import remove_ir
 import warnings
 
-def get_sig_nois_data(fname,origtime,Atime,Btime,time_win,wv_fig,phase):
+def get_sig_nois_data(self,fname,origtime,Atime,Btime,time_win,wv_fig,phase,evid,baz,rmv_instr_resp):
     st = read(fname)
     if Atime:
         Atime = Atime - max(0.05,st[0].stats.delta)  
@@ -21,88 +22,71 @@ def get_sig_nois_data(fname,origtime,Atime,Btime,time_win,wv_fig,phase):
         comp2,comp3 = comp1[0:2]+'N',comp1[0:2]+'Z'
     elif 'N' in comp1:
         comp2,comp3 = comp1[0:2]+'E',comp1[0:2]+'Z' 
-    nss = None
-    if wv_fig is not True:
+    try:
+        st += read(fname.replace(comp1,comp2))
+    except:
+        warnings.warn('Could not read %s channel for %s' % \
+                          (comp2,st[0].stats.station),UserWarning)
+        pass
+    try:
+        st += read(fname.replace(comp1,comp3))
+    except:
+        warnings.warn('Could not read %s channel for %s' % \
+                          (comp3,st[0].stats.station),UserWarning)
+        pass
+    st.detrend(type='linear')
+    st.detrend(type='demean')
+    st.taper(max_percentage=0.05)
+    if wv_fig is not True:  
+        sts = st.copy()       
+        if self.method in [2,3] and st[0].stats.channel[1:3] in ['NN','NE','NZ'] and rmv_instr_resp is False:
+            sts.integrate().integrate()
+        elif self.method in [2,3] and st[0].stats.channel[1:3] in ['HN','HE','HZ'] and rmv_instr_resp is False:
+            sts.integrate()
+        st = remove_ir(self,st,baz,evid,'DISP')
+        ns = st.copy()
+        nss = sts.copy() 
         if phase == 'S':
-            sts = read(fname,starttime = Btime, endtime = Btime + time_win)
+            sts.trim(starttime = Btime, endtime = Btime + time_win)
+            st.trim(starttime = Btime, endtime = Btime + time_win)
         elif phase == 'P':
-            sts = read(fname,starttime = Atime, endtime = Atime + time_win)    
+            sts.trim(starttime = Atime, endtime = Atime + time_win)
+            st.trim(starttime = Atime, endtime = Atime + time_win)
         if not Atime and phase == 'S':
-            nss  = read(fname,starttime = st[0].stats.starttime + 1. ,\
+            nss.trim(starttime = st[0].stats.starttime + 1. ,\
+                        endtime = st[0].stats.starttime + 1.+time_win)
+            ns.trim(starttime = st[0].stats.starttime + 1. ,\
                         endtime = st[0].stats.starttime + 1.+time_win)
             nsstart = st[0].stats.starttime + 1.
         elif Atime and phase == 'S':
-            nss  = read(fname,starttime = Atime - (time_win+1) ,\
+            nss.trim(starttime = Atime - (time_win+1) ,\
+                        endtime = Atime - 1) 
+            ns.trim(starttime = Atime - (time_win+1) ,\
                         endtime = Atime - 1) 
             nsstart = Atime - (time_win+1)
         elif phase == 'P':
-            nss  = read(fname,starttime = Atime - (time_win+1) ,\
+            nss.trim(starttime = Atime - (time_win+1) ,\
+                        endtime = Atime - 1) 
+            ns.trim(starttime = Atime - (time_win+1) ,\
                         endtime = Atime - 1) 
             nsstart = Atime - (time_win+1)
-        try:
-            if phase == 'S':
-                sts += read(fname.replace(comp1,comp2),starttime = Btime, endtime = Btime + time_win)
-            elif phase == 'P':
-                sts += read(fname.replace(comp1,comp2),starttime = Atime, endtime = Atime + time_win)    
-#            sts += read(fname.replace(comp1,comp2),starttime = Atime, \
-#                        endtime = Atime + time_win)
-            if not Atime and phase == 'S':
-                nss  += read(fname.replace(comp1,comp2),starttime = \
-                             st[0].stats.starttime + 1. ,endtime = \
-                             st[0].stats.starttime + 1.+time_win)
-            elif Atime and phase == 'S':
-                nss  += read(fname.replace(comp1,comp2),starttime = \
-                             Atime - (time_win+1) ,endtime = Atime - 1)
-            elif phase == 'P':
-                nss  += read(fname.replace(comp1,comp2),starttime = Atime - \
-                            (time_win+1) ,endtime = Atime - 1) 
-        except:
-            warnings.warn('Could not read east channel for %s' % \
-                          sts[0].stats.station,
-                      UserWarning)
-            pass
-        try:
-            if phase == 'S':
-                sts += read(fname.replace(comp1,comp3),starttime = Btime, endtime = Btime + time_win)
-            elif phase == 'P':
-                sts += read(fname.replace(comp1,comp3),starttime = Atime, endtime = Atime + time_win)    
-#            sts += read(fname.replace(comp1,comp2),starttime = Atime, \
-#                        endtime = Atime + time_win)
-            if not Atime and phase == 'S':
-                nss  += read(fname.replace(comp1,comp3),starttime = \
-                             st[0].stats.starttime + 1. ,endtime = \
-                             st[0].stats.starttime + 1.+time_win)
-            elif Atime and phase == 'S':
-                nss  += read(fname.replace(comp1,comp3),starttime = \
-                             Atime - (time_win+1) ,endtime = Atime - 1)
-            elif phase == 'P':
-                nss  += read(fname.replace(comp1,comp3),starttime = Atime - \
-                            (time_win+1) ,endtime = Atime - 1) 
-        except:
-            warnings.warn('Could not read east channel for %s' % \
-                          sts[0].stats.station,
-                      UserWarning)
-            pass
     elif wv_fig is True:
+        sts = None
+        nss = None
+        ns = None
+        st = remove_ir(self,st,baz,evid,'VEL')
         nsstart = None
         time_win_add = 5.0
+        sts
         if Btime and not Atime:    
-            sts = read(fname,starttime = origtime,endtime = \
+            st.trim(starttime = origtime,endtime = \
                        (Btime + time_win+time_win_add+.1))
-            sts += read(fname.replace(comp1,comp2),starttime = \
-                        origtime,endtime = (Btime + time_win+time_win_add+.1))
-            sts += read(fname.replace(comp1,comp1),starttime = \
-                        origtime,endtime = (Btime + time_win+time_win_add+.1))
             nsstart = sts[0].stats.starttime + 1.
         elif Atime and Btime:
             time_win_add = (Btime - Atime)*2
-            sts = read(fname,starttime = (Atime -(time_win+2)),endtime = (Btime + time_win+time_win_add+.1))
-            sts += read(fname.replace(comp1,comp2),starttime = (Atime - (time_win+2)),endtime = (Btime + time_win+time_win_add+.1))
-            sts += read(fname.replace(comp1,comp3),starttime = (Atime - (time_win+2)),endtime = (Btime + time_win+time_win_add+.1))
+            st.trim(starttime = (Atime -(time_win+2)),endtime = (Btime + time_win+time_win_add+.1))
             nsstart = Atime - (time_win+1)
         elif  Atime and not Btime :
-            sts = read(fname,starttime = (Atime - (time_win+2)),endtime = (Atime + time_win+time_win_add+.1))
-            sts += read(fname.replace(comp1,comp2),starttime = (Atime - (time_win+2)),endtime = (Atime + time_win+time_win_add+.1))
-            sts += read(fname.replace(comp1,comp3),starttime = (Atime - (time_win+2)),endtime = (Atime + time_win+time_win_add+.1))
+            st.trim(starttime = (Atime - (time_win+2)),endtime = (Atime + time_win+time_win_add+.1))
             nsstart = Atime - (time_win+1)
-    return sts,nss,nsstart
+    return sts,nss,nsstart,st,ns
--- src/analyzer/remove_ir.py
+++ src/analyzer/remove_ir.py
@@ -8,9 +8,9 @@ Created on Mon Apr 22 10:20:52 2019
 from obspy.core import Stream
 from .free_surface_correction import free_surface_correction
 
-def remove_ir(self,st,ns,baz,evid,output):
+def remove_ir(self,st,baz,evid,output):
     trn2 = Stream()
-    respf = self.stationxml#read_inventory(self.maindir+'/input/stations.xml',format='STATIONXML')
+    respf = self.stationxml
     try:
         trn2 = st.select(component='N')
     except:
@@ -38,33 +38,4 @@ def remove_ir(self,st,ns,baz,evid,output):
         if self.freesurface_cor is True:
             st = free_surface_correction(self,st,evid)
         trn2.rotate('RT->NE',back_azimuth=baz)
-    if ns:
-        trn2 = Stream()
-        try:
-            trn2 = ns.select(component='N')
-        except:
-            pass
-        try:
-            trn2 += ns.select(component='E')
-        except:
-            pass
-        pre_filt = self.stationlist[ns[0].stats.station.strip()]['pre_filt']
-#        pre_filt = self.stationlist[ns[0].stats.station.strip()][ns[0].stats.channel.strip()]
-        if pre_filt:
-            for tr in ns:
-                tr.remove_response(respf,pre_filt=pre_filt,output = output)
-        else:
-            for tr in ns:
-                print('no prefilt')
-                tr.remove_response(respf,output = output)
-        if len(trn2) == 2:
-            trn2.rotate('NE->RT',back_azimuth=baz)
-            for tr in ns:
-                if tr.stats.channel.strip()[-1] == 'N':
-                    tr = trn2[0]
-                elif tr.stats.channel.strip()[-1] == 'E':
-                    tr = trn2[1]
-            if self.freesurface_cor is True:
-                ns = free_surface_correction(self,ns,evid)
-            trn2.rotate('RT->NE',back_azimuth=baz)
-    return st,ns
+    return st
