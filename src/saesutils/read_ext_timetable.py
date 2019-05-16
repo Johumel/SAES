e09d8d514bd71f256b2d5d46419e759964926ead
John Onwuemeka <john.onwuemeka@Johns-MacBook-Air.local>
2019-05-16 07:11:55 -0400

    update non-pyrocko timetable reader

--- src/saesutils/read_ext_timetable.py
+++ src/saesutils/read_ext_timetable.py
@@ -8,10 +8,16 @@ Created on Mon Apr 22 08:34:52 2019
 import numpy as np 
 
 def read_ext_timetable(self):
-    data = np.genfromtxt(self.maindir+'/input/tt.dat',dtype=['U16','U16','f8','f8'])
+    #time table format --> eventID station timepick phasename(P,S)
+    data = np.genfromtxt(self.maindir+'/input/tt.dat',dtype=['U16','U16','f8','U16'])
     evid_tt = [data[i][0] for i in range(len(data))]
     P_tt,S_tt = {},{}
-    for i in range(len(data)): P_tt[evid_tt[i]].append([data[i][2],data[i][1]])
-    for i in range(len(data)): S_tt[evid_tt[i]].append([data[i][3],data[i][1]])
-    evid_tt = sorted(list(set(evid_tt)))
-    return P_tt,S_tt,evid_tt
+    for i in range(len(data)):
+        if data[i][3].upper() == 'P':
+            P_tt[evid_tt[i]].append([data[i][2],data[i][1]]) 
+        elif data[i][3].upper() == 'S':
+            S_tt[evid_tt[i]].append([data[i][2],data[i][1]])
+#    evid_tt = sorted(list(set(evid_tt)))
+    self.P_tt = P_tt
+    self.S_tt = S_tt
+    return None
