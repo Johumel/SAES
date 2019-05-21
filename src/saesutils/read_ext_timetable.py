5a8ad4828157dcec38ad290afcede2e84887b8fa
Johumel <john.onwuemeka@gmail.com>
2019-05-21 13:37:55 -0400

    Update read_ext_timetable.py

--- src/saesutils/read_ext_timetable.py
+++ src/saesutils/read_ext_timetable.py
@@ -6,17 +6,18 @@ Created on Mon Apr 22 08:34:52 2019
 @author: john.onwuemeka
 """
 import numpy as np 
+from obspy.core import UTCDateTime
 
 def read_ext_timetable(self):
     #time table format --> eventID station timepick phasename(P,S)
-    data = np.genfromtxt(self.maindir+'/input/tt.dat',dtype=['U16','U16','f8','U16'])
+    data = np.genfromtxt(self.maindir+'/input/tt.dat',dtype=['U24','U12','U32','U2'])
     evid_tt = [data[i][0] for i in range(len(data))]
     P_tt,S_tt = {},{}
     for i in range(len(data)):
         if data[i][3].upper() == 'P':
-            P_tt[evid_tt[i]].append([data[i][2],data[i][1]]) 
+            P_tt[evid_tt[i]].append([UTCDateTime(data[i][2]),data[i][1]]) 
         elif data[i][3].upper() == 'S':
-            S_tt[evid_tt[i]].append([data[i][2],data[i][1]])
+            S_tt[evid_tt[i]].append([UTCDateTime(data[i][2]),data[i][1]])
 #    evid_tt = sorted(list(set(evid_tt)))
     self.P_tt = P_tt
     self.S_tt = S_tt
