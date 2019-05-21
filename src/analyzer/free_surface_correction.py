e852f83beb9b4e60d6439221470d82981d934a5d
Ge Li <ge.li2@mail.mcgill.ca>
2019-05-21 10:51:11 -0400

    Update free_surface_correction.py

--- src/analyzer/free_surface_correction.py
+++ src/analyzer/free_surface_correction.py
@@ -12,13 +12,22 @@ Taumo = TauPyModel(model="iasp91")
 from obspy.geodetics import locations2degrees as loc2deg
 import warnings
 
-    
+
 def free_surface_correction(self,stream,evid):
-    ''' This section computes free surface correction, which mostly 
-        affect surface station, based on the method in Aki & Richards, 1980;
-        House, L. and Boatwright, J., 1980; Kennett, B.L.N., 1991; Kim et al., 
-        ?? '''
-        
+
+    """
+    This section computes free surface correction, which mostly
+    affect surface station, based on the method in Aki & Richards, 1980;
+    House, L. and Boatwright, J., 1980; Kennett, B.L.N., 1991; Kim et al., ??
+
+    Parameters:
+    stream (obspy stream): Input 3 component waveform data
+    evid (int): Event id.
+
+    Returns:
+    int: Description of return value
+
+    """
 
     tr = stream.select(component='T')
     tr += stream.select(component='R')
@@ -26,12 +35,12 @@ def free_surface_correction(self,stream,evid):
     evid = str(evid)
     evid = int(evid)#[3:12])
     station = self.stationxml.get_coordinates(stream[0].stats.network.strip()+'.'+stream[0].stats.station.strip()+'..'+\
-                        stream[0].stats.channel.strip(), stream[0].stats.starttime) 
+                        stream[0].stats.channel.strip(), stream[0].stats.starttime)
     latst = station['latitude']; lonst = station['longitude']
     latev = self.evlist[evid][1]
     lonev = self.evlist[evid][2]
     depthev = self.evlist[evid][3]
-        
+
     # Assumin Vp = 4.8km/s and a poisoon solid Vp/Vs = 1.73
     try:
         if self.freesurface_vp is not None:
@@ -39,7 +48,7 @@ def free_surface_correction(self,stream,evid):
     except:
         Vp = 4.8
         pass
- 
+
     try:
         if self.freesurface_vs is not None:
             Vs = self.freesurface_vs
@@ -60,17 +69,17 @@ def free_surface_correction(self,stream,evid):
             C = (1 - (2*Vs**2*rayparam**2))/(2*(np.sqrt(1-(Vp**2*rayparam**2))))
             D = Vs**2*rayparam/Vp
             if rayparam < 1/Vp:
-                trp = np.add(np.multiply(Z,C),np.multiply(D,R))  
+                trp = np.add(np.multiply(Z,C),np.multiply(D,R))
                 trv = np.subtract(np.multiply(A,R),np.multiply(B,Z))
                 trh = np.multiply(T,0.5)
             elif rayparam >= 1/Vp:
                 Z = np.imag(hilbert(Z))
-                trp = np.add(np.multiply(Z,C),np.multiply(D,R))  
+                trp = np.add(np.multiply(Z,C),np.multiply(D,R))
                 trv = np.subtract(np.multiply(A,R),np.multiply(B,Z))
                 trh = np.multiply(T,0.5)
             stream.select(component='R')[0].data = trv
             stream.select(component='T')[0].data = trh
-            stream.select(component='Z')[0].data = trp 
+            stream.select(component='Z')[0].data = trp
     else:
         warnings.warn('Could not determine ray parameter for %s at %s' %(evid,stream[0].stats.station.strip()),
                       UserWarning)
